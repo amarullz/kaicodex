d85fe7f8c5be00dd98b9c39a020dc573dd91d25e
Ahmad Amarullah <amarullz@gmail.com>
2025-04-07 01:12:09 +0700

    Add license on index.js & disable json format

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2025 amarullz.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *______________________________________________________________________________
+ *
+ * Filename    : index.js
+ * Description : Main code
+ *
+ */
 import decodex from './decodex.js';
 import codexhelper from './codexhelper.js';
 import fs from 'fs';
@@ -203,7 +223,7 @@ let allCodex={
 };
 
 console.log("All Codex Sucessfully generated!");
-fs.writeFileSync("./generated/kai_codex.json",JSON.stringify(allCodex,null,2));
+fs.writeFileSync("./generated/kai_codex.json",JSON.stringify(allCodex));
 
 console.log("Generating KaiCodex JS Class...");
 let template=`const KAICODEX = {
